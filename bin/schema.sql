26d4a9ff31e95265e21af3408fe260dadf123d21
ccharris <ccharris27@gmail.com>
2016-11-10 13:04:59 -0500

    postrgress

--- bin/schema.sql
+++ bin/schema.sql
@@ -1,18 +1,17 @@
 CREATE SCHEMA IF NOT EXISTS giftOrganizer;
-USE giftOrganizer ;
 
 CREATE TABLE IF NOT EXISTS giftOrganizer.users (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   first_name VARCHAR(45) NOT NULL,
   last_name VARCHAR(45) NOT NULL,
   password VARCHAR(45) NOT NULL,
   email VARCHAR(128) NOT NULL,
-  active tinyint(1) NOT NULL DEFAULT 1,
+  active boolean NOT NULL DEFAULT true,
   role VARCHAR(45) NOT NULL,
   PRIMARY KEY (id));
   
   CREATE TABLE IF NOT EXISTS giftOrganizer.recipients (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   first_name VARCHAR(45) NOT NULL,
   last_name VARCHAR(45) NULL,
   email VARCHAR(128) NULL,
@@ -22,24 +21,24 @@ CREATE TABLE IF NOT EXISTS giftOrganizer.users (
   PRIMARY KEY (id));
   
   CREATE TABLE IF NOT EXISTS giftOrganizer.events (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   name VARCHAR(45) NOT NULL,
   budget DECIMAL(8,2) NULL,
   user_id INT NOT NULL,
   PRIMARY KEY (id));
   
   CREATE TABLE IF NOT EXISTS giftOrganizer.event_recipients (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   recipient_id INT NOT NULL,
   event_id INT NOT NULL,
   PRIMARY KEY (id));
   
   CREATE TABLE IF NOT EXISTS giftOrganizer.gifts (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   name VARCHAR(200) NOT NULL,
   recipient_id INT NOT NULL,
   price DECIMAL(6,2) NULL,
-  bought tinyint(1) NOT NULL DEFAULT 0,
+  bought boolean NOT NULL DEFAULT false,
   link_one VARCHAR(500) NULL,
   link_two VARCHAR(500) NULL,
   PRIMARY KEY (id));
